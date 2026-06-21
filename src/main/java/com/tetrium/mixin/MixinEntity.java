88927b76fdb62cba9cd07fc197ec15f4906bc686
saptok134 <esaha8693@gmail.com>
2026-06-21 17:13:36 +0600

    Create MixinEntity class for entity culling optimization

---
+++ src/main/java/com/tetrium/mixin/MixinEntity.java
@@ -0,0 +1,10 @@
+package com.tetrium.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.entity.Entity;
+import com.tetrium.config.TetriumConfig;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+    // Entity culling optimization placeholder
+}
