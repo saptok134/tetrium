47456039eadd918cf62b8c4da720123eeacb2cbc
saptok134 <esaha8693@gmail.com>
2026-06-21 17:14:10 +0600

    Add Mixin for BlockEntity with optimization placeholder

---
+++ src/main/java/com/tetrium/mixin/MixinBlockEntity.java
@@ -0,0 +1,10 @@
+package com.tetrium.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.block.entity.BlockEntity;
+import com.tetrium.config.TetriumConfig;
+
+@Mixin(BlockEntity.class)
+public class MixinBlockEntity {
+    // Block entity culling optimization placeholder
+}
